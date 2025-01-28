58fb9d0d19d0ff0aea5f3691512621dc934bc1d4
KrishanKumarSaini <sainishubh1699@gmail.com>
2025-01-28 22:28:16 +0530

    added formHandling function

--- script.js
+++ script.js
@@ -1,44 +1,93 @@
 const myLibrary = [];
 
-function Book(name, author, pages){
+function Book(name, author, pages) {
     this.name = name;
     this.author = author;
     this.pages = pages;
 }
 
-function addBookToLibrary(){
-    const book1 = new Book("Book1","author1",100);
-    const book2 = new Book("Book2","author2",200);
+function addBookToLibrary(formName, formAuthor, formPages) {
+    const newbook = new Book(formName, formAuthor, formPages);
 
-    myLibrary.push(book1,book2);
+    myLibrary.push(newbook);
 }
 
-function displayBooks(){
-    addBookToLibrary();
-    const BookNo = document.getElementsByClassName("BookNo");
-    const BookName = document.getElementsByClassName("BookName");
-    const BookAuthor = document.getElementsByClassName("BookAuthor");
-    const BookPages = document.getElementsByClassName("BookPages")
+function displayBooks() {
+    
+    const tableBody = document.querySelector("table tbody");
 
-    for(let i=0; i<myLibrary.length; i++){
-        if(BookNo){
-            BookNo[i].innerHTML = i+1;
-        }
-        if(BookName){
-            BookName[i].innerHTML = myLibrary[i].name;
-        }
-        if(BookAuthor){
-            BookAuthor[i].innerHTML = myLibrary[i].author;
-        }
-        if(BookPages){
-            BookPages[i].innerHTML = myLibrary[i].pages;
-        }
+    while(tableBody.firstChild){
+        tableBody.removeChild(tableBody.firstChild);
     }
 
+
+    for (let i = 0; i < myLibrary.length; i++) {
+
+        const book = myLibrary[i];
+
+        const row = document.createElement("tr");
+        const bookN0 = document.createElement("td");
+        bookN0.className = "BookNo";
+        bookN0.textContent = i+1;
+
+        const bookName = document.createElement("td");
+        bookName.className = "BookName";
+        bookName.textContent = book.name;
+
+        const bookAuthor = document.createElement("td");
+        bookAuthor.className = "BookAuthor";
+        bookAuthor.textContent = book.author;
+
+        const bookPages = document.createElement("td");
+        bookPages.className = "BookPages";
+        bookPages.textContent = book.pages;
+
+        row.appendChild(bookN0);
+        row.appendChild(bookName);
+        row.appendChild(bookAuthor);
+        row.appendChild(bookPages);
+
+        tableBody.appendChild(row);
+    }
 }
 
+
+const newBookBtn = document.getElementById("newBookBtn");
+
+newBookBtn.addEventListener('click', function () {
+    const formContainer = document.getElementById('formContainer');
+
+    if (formContainer.style.display === 'none') {
+        formContainer.style.display = 'block';
+
+        const form = document.getElementById("bookForm");
+        const outputDiv = document.getElementById("outputDiv");
+
+        form.addEventListener('submit', function(event){
+            event.preventDefault();
+
+            const formData = new FormData(form);
+            const formName = formData.get('Name');
+            const formAuthor = formData.get('Author');
+            const formPages = formData.get('Pages');
+            
+            addBookToLibrary(formName, formAuthor, formPages);
+            displayBooks();
+
+            form.reset();
+        });
+
+
+    } else {
+        formContainer.style.display = 'none';
+    }
+});
+
+
+
+
 document.addEventListener('DOMContentLoaded',
-    function(){
+    function () {
         displayBooks();
     }
 );
